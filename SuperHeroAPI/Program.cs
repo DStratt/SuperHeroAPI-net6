004098574c83547090535b62c4224d19a89b7930
diane.e.stratton <diane.stratton@dodiis.mil>
2023-09-11 08:27:32 -0400

    Add project files.

---
+++ SuperHeroAPI/Program.cs
@@ -0,0 +1,50 @@
+using Microsoft.EntityFrameworkCore;
+using SuperHeroAPI.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+//builder.Services.AddCors();
+
+
+// Add if CORS Error observed (part 1)
+builder.Services.AddCors(options => options.AddPolicy("SuperHeroOrigins",
+    policy =>
+    {
+        policy
+            .WithOrigins("http://localhost:4200")
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    }));
+
+var app = builder.Build();
+
+// Add if CORS Error observed (part 2) - this must be first thing listed in the chain of app calls
+app.UseCors("SuperHeroOrigins");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
